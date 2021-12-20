7b046a2a0a18fbc085ab03fd18a12b96b058112e
OrtizBrayan <brayanortizbastidas@gmail.com>
2021-12-20 04:07:49 -0500

    Changed painted piece methos and added delete piece method

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Tetris
     {
         const int INITIAL_POSITION = 4;
         private Piece currentPiece;
-
+        private Rectangle currentBlock;
         public MainWindow()
         {
             
@@ -43,55 +43,127 @@ namespace Tetris
                 {
                     if(form[i,j] != currentPiece.DEFECT_VALUE)
                     {
-                        Rectangle rect = new Rectangle
-                        {
-                            Width = 45,
-                            Height = 45
-                        };
-                        SolidColorBrush c = new SolidColorBrush
-                        {
-                            Color = Color.FromArgb(255, 255, 255, 0)
-                        };
-                        rect.Fill = c;
-                        Grid.SetColumn(rect, INITIAL_POSITION + i);
-                        Grid.SetRow(rect, j);
+                        PaintPiece(INITIAL_POSITION + i, j);
                         currentPiece.CordenadesX.Add(INITIAL_POSITION + i);
                         currentPiece.CordenadesY.Add(j);
-                        grid.Children.Add(rect);
                     }
                 }
                 
             }
         }
 
+        private void PaintPiece(int x, int y)
+        {
+            currentBlock = new Rectangle()
+            {
+                Width = 45,
+                Height = 45
+            };
+            SolidColorBrush c = new SolidColorBrush();
+            switch (currentPiece.Id)
+            {
+                case 0:
+                    c.Color = Color.FromRgb(0,255,255);
+                    break;
+
+                case 1:
+                    c.Color = Color.FromRgb(138, 43, 226);
+                    break;
+
+                case 2:
+                    c.Color = Color.FromRgb(255, 165, 0);
+                    break;
+
+                case 3:
+                    c.Color = Color.FromRgb(0, 0, 255);
+                    break;
+
+                case 4:
+                    c.Color = Color.FromRgb(124, 252, 0);
+                    break;
+
+                case 5:
+                    c.Color = Color.FromRgb(128, 0, 128);
+                    break;
+
+                case 6:
+                    c.Color = Color.FromRgb(255, 0, 0);
+                    break;
+            }
+            currentBlock.Fill = c;
+            Grid.SetColumn(currentBlock, x);
+            Grid.SetRow(currentBlock, y);
+            grid.Children.Add(currentBlock);
+        }
+
+        private void DeletePiece(int x, int y)
+        {
+            currentBlock = new Rectangle()
+            {
+                Width = 47,
+                Height = 47
+            };
+            SolidColorBrush c = new SolidColorBrush()
+            {
+                Color = Color.FromRgb(0,0,0)
+            };
+            currentBlock.Fill = c;
+            Grid.SetColumn(currentBlock, x);
+            Grid.SetRow(currentBlock, y);
+            grid.Children.Add(currentBlock);
+        }
+
         public void PieceDown()
         {
-            grid.Children.Clear();
-            int[,] current = currentPiece.Form;
+            int yInit = currentPiece.CordenadesY[0];
             for (int i = 0; i < currentPiece.CordenadesX.Capacity; i++)
             {
                 int x = currentPiece.CordenadesX[i];
                 int y = currentPiece.CordenadesY[i];
-                Rectangle rect = new Rectangle();
-                rect.Width = 45;
-                rect.Height = 45;
-                SolidColorBrush c = new SolidColorBrush();
-                c.Color = Color.FromArgb(255, 255, 255, 0);
-                rect.Fill = c;
-                Grid.SetColumn(rect, x);
-                Grid.SetRow(rect, y + 1);
+                PaintPiece(x, y + 1);
+                DeletePiece(x, y);
                 currentPiece.CordenadesY[i] = y + 1;
-                grid.Children.Add(rect);
             }
         }
 
-        private void MovePieceDown(object sender, KeyEventArgs e)
+        public void PieceLeft()
+        {
+            grid.Children.Clear();
+            for (int i = 0; i < currentPiece.CordenadesX.Capacity; i++)
+            {
+                int x = currentPiece.CordenadesX[i];
+                int y = currentPiece.CordenadesY[i];
+                PaintPiece(x - 1, y);
+                currentPiece.CordenadesX[i] = x - 1;
+            }
+        }
+
+        public void PieceRigth()
+        {
+            grid.Children.Clear();
+            for (int i = 0; i < currentPiece.CordenadesX.Capacity; i++)
+            {
+                int x = currentPiece.CordenadesX[i];
+                int y = currentPiece.CordenadesY[i];
+                PaintPiece(x + 1, y);
+                currentPiece.CordenadesX[i] = x + 1;
+            }
+        }
+
+        private void MovePiece(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Down)
             {
                 PieceDown();
+            } else if (e.Key == Key.Right)
+            {
+                PieceRigth();
+            
+            }else if (e.Key == Key.Left)
+            {
+                PieceLeft();
             }
-            if (e.Key == Key.Space)
+            else if (e.Key == Key.Space)
             {
                 GenerateAndPaintPiece();
             }
